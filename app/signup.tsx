2bb49c0b5148b5e86cae74ae6ba515a82233b5a7
Scott Watanuki <scottwatanuki@gatech.edu>
2025-03-08 18:11:42 -0500

    Refactoring logic for signup.tsx

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Signup from "./screens/Signup";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 
 export default function SignupScreen() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={styles.container}>
       <Signup />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
